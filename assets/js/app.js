244f93ef5e680f22920bcc142922ffa0eddf0bc5
erimozcancivgin <erimozcnacivgin@gmail.com>
2023-03-01 22:04:55 +0300

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,60 +1,117 @@
 import { UI } from "./ui.js";
-import { Request } from "./request.js";
-const request = new Request();
 const ui = new UI();
 
 // Setup module
 // ------------------------------
 
 const ecommerceBasic = (function () {
-  const addTodoModalTrigger = document.querySelector(".js-add-todo-button");
-  const newTodoModal = document.querySelector(".js-add-todo-modal")
-  const addNewTodo = document.querySelector(".js-new-todo")
-  const listBody = document.querySelector(".js-list-body")
+  const url = "http://localhost:3000/todoList/"
   //
   // Setup module components
   //
 
   // Hover Effect
   const _eventListeners = function () {
-    addTodoModalTrigger.addEventListener("click", _triggerNewTodoModal)
-    newTodoModal.addEventListener("click", _closeNewTodoModal)
-    addNewTodo.addEventListener("submit", _addNewTodoFromModal)
-
+    $(document).on("click", ".js-add-todo-button", _triggerNewTodoModal)
+    $(document).on("click", ".js-add-todo-modal", _closeNewTodoModal)
+    $(document).on("submit", ".js-new-todo", _addNewTodoFromModal)
+    $(document).on("click", ".js-remove-todo", _removeTodoFromList)
+    $(document).on("click", ".js-update-todo", _editTodoFromList)
+    $(document).on('keydown', function (event) {
+      if (event.key == "Escape" || event.key == 27) {
+        $(".js-add-todo-modal").removeClass("active")
+          ;
+      }
+    });
 
   };
+  const _removeTodoFromList = function () {
+    console.log("asdgy"); $.ajax({
+      url: url + $(this).attr("data-id"),
+      type: "DELETE",
+      dataType: "json",
+      success: function (data) {
+        _getTodos()
+      }
+    });
+  }
+
+  const _editTodoFromList = function () {
+    const firstName = $(this).closest("ul").find("[data-first-name]")
+    const lastName = $(this).closest("ul").find("[data-last-name]")
+    const age = $(this).closest("ul").find("[data-age]")
+    const gender = $(this).closest("ul").find("[data-gender]")
+    console.log(firstName.attr("data-first-name"));
+
+    $(".js-new-todo").attr("data-id", $(this).attr("data-id"))
+    $('[name="first_name"]').val(firstName.attr("data-first-name"))
+    $('[name="last_name"]').val(lastName.attr("data-last-name"))
+    $('[name="age"]').val(age.attr("data-age"))
+    $('[name="gender"]').val(gender.attr("data-gender"))
+    $(".js-add-todo-modal").addClass("active")
+    $(".js-add-new-todo").addClass("update")
+  }
+
 
   const _triggerNewTodoModal = function () {
-    newTodoModal.classList.add("active")
-
+    $(".js-add-todo-modal").addClass("active")
+    $(".js-add-new-todo").removeClass("update")
   }
+
   const _closeNewTodoModal = function (e) {
-    console.log(e.target.classList.contains(".js-add-todo-modal"));
     if (e.target === this) {
-      newTodoModal.classList.remove("active")
+      $(".js-add-todo-modal").removeClass("active")
     }
   }
+
   const _addNewTodoFromModal = function (e) {
     e.preventDefault()
-    request.delete("http://localhost:3000/todoList/2").then((data) => {
-      console.log(data);
-    })
+
     let formValues = {}
-    // const newTodoForm = this
-    // formValues.first_name = newTodoForm.querySelector('[name="first_name"]').value
-    // formValues.last_name = newTodoForm.querySelector('[name="last_name"]').value
-    // formValues.age = newTodoForm.querySelector('[name="age"]').value
-    // formValues.gender = newTodoForm.querySelector('[name="gender"]').value
+    formValues.first_name = $(this).find('[name="first_name"]').val()
+    formValues.last_name = $(this).find('[name="last_name"]').val()
+    formValues.age = $(this).find('[name="age"]').val()
+    formValues.gender = $(this).find('[name="gender"]').val()
     if (formValues.first_name && formValues.last_name && formValues.age && formValues.gender) {
+      if ($(".js-add-new-todo").hasClass("update")) {
+        $.ajax({
+          url: url + $(this).attr("data-id"),
+          type: "PUT",
+          data: formValues,
+          dataType: "json",
+          success: function (data) {
+            _getTodos()
+          }
+        });
+      }
+      else {
+        $.ajax({
+          url: url,
+          type: "POST",
+          data: formValues,
+          dataType: "json",
+          success: function (data) {
+            _getTodos()
+          }
+        });
+      }
+
     }
 
   }
-  const _getTodos = function () {
-    request.get("http://localhost:3000/todoList").then((data) => {
-      ui.setTodo(listBody, data)
-      
-    })
 
+
+  const _getTodos = function () {
+    // $.get(URL,callback)
+    ui.resetNewTodoModalUI()
+    $.ajax({
+      url: url,
+      type: "GET",
+      dataType: "json",
+      success: function (data) {
+        ui.setTodo($(".js-list-body"), data);
+      }
+    });
   }
 
   //
@@ -64,7 +121,6 @@ const ecommerceBasic = (function () {
   return {
     init: function () {
       _eventListeners();
-      console.log(request);
       _getTodos()
     },
   };
--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,22 +1,32 @@
 export class UI {
 
   setTodo(element, item) {
-    element.innerHTML = ""
+    element.html("")
     item.forEach(item => {
-      element.innerHTML += `
-    <ul>
-    <li>${item.first_name}</li>
-    <li>${item.last_name}</li>
-    <li>${item.age}</li>
-    <li>${item.gender}</li>
-    <li>
-        <button type="button"> delete</button>
-        <button type="button"> update</button>
-    </li>
-</ul>`
+      element.append(`
+                  <ul>
+                  <li data-first-name="${item.first_name}">${item.first_name}</li>
+                  <li data-last-name="${item.last_name}">${item.last_name}</li>
+                  <li data-age="${item.age}">${item.age}</li>
+                  <li data-gender="${item.gender}">${item.gender}</li>
+                  <li>
+                      <button type="button" data-id="${item.id}" class="js-remove-todo"> delete</button>
+                      <button type="button" data-id="${item.id}" class = "js-update-todo"> update</button>
+                  </li>
+              </ul>`
+      )
     });
-
+  }
+  resetNewTodoModalUI(){
+    $('[name="first_name"]').val("")
+    $('[name="last_name"]').val("")
+    $('[name="age"]').val("")
+    $('[name="gender"]').val("")
+    $(".js-add-todo-modal").removeClass("active")
 
 
   }
+
+
+
 }
